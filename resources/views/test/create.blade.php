449c1a1f7ba2683e6a7147306715c45d5890f1f7
Thel Nu Aein <thelnuaein5450.com>
2022-01-14 09:57:48 +0630

    tast update

---
+++ resources/views/test/create.blade.php
@@ -0,0 +1,22 @@
+@extends('master')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="{{route('test.store')}}" method="post">
+                    @csrf
+                    <div class="col-12 col-lg-4">
+                        <label for="" class="form-label">Category Name</label>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <input type="text" class="form-control" name="title">
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <button class="btn btn-outline-primary">Create</button>
+                    </div>
+                </form>
+                @include('test.list')
+            </div>
+        </div>
+    </div>
+@endsection
